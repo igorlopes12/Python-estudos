433001b250bc87e2ca9b905839c3985f1407244f
Igor Moura Lopes <82513750+igorlopes12@users.noreply.github.com>
2022-05-26 17:52:27 -0300

    Add files via upload
    Versão mais atualizada dos jogos desenvolvidos no curso de Python 3 da Alura

--- forca.py
+++ forca.py
@@ -3,9 +3,37 @@ def jogar():
     print('Bem vindo a Forca')
     print('*' * 30)
 
-    palavra_secreta = 'banana'
+    palavra_secreta = 'banana'.upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
 
-    while()
+    enforcou = False
+    acertou = False
+    tentativas = 0
+
+    # enquanto(true):
+    while (not enforcou and not acertou):
+
+        chute = input("Qual a letra? ")
+        chute = chute.strip().upper()
+
+        if (chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if (chute.upper() == letra.upper()):
+                    letras_acertadas[index] = letra
+                index = index + 1
+        else:
+            tentativas += 1
+        enforcou = tentativas == 6
+        acertou = "_" not in letras_acertadas
+        print("Você errou, ainda há {} tentativas".format(6 - tentativas))
+        print(letras_acertadas)
+
+    if (acertou):
+        print("Você ganhou!!")
+
+    else:
+        print("Você perdeu! ")
 
     print("Fim do jogo")
 
